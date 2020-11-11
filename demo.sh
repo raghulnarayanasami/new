4e45b520fac05abb3f318b8769d17d9057440c23
raghulnarayanasami <58605993+raghulnarayanasami@users.noreply.github.com>
2020-11-11 00:26:34 -0500

    updated startup script

--- demo.sh
+++ demo.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-cd /root
-source env/bin/activate
-cd /root/demoproject
-kill -9 $(lsof -ti :8001)
-nohup python manage.py runserver 0.0.0.0:8001 >> /root/demoproject/event.log &
-echo 'Done'
-echo 'Done'
+virtualenv -p /usr/bin/python3 env3
+cd /root/
+source env3/bin/activate
+pip install django
+pip install setuptools==49.3.0
+pip install boto3
+cd demoproject
+nohup python3 manage.py runserver localhost:8000 &
